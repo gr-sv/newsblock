2de99ab437a8d04a539d6f4e50b4c31a20026105
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-04-17 23:10:00 +0300

    Add Flex elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Flex, Typography } from 'antd';
+import { data } from './data.js';
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+     <Flex vertical>
+		<Flex vertical='horizontal'>
+			<time datetime={data?.DP}>{data?.DP}</time>
+			<div>Reach</div>
+			<div>Top Traffic</div>
+			<div>Sentiment</div>
+			<div>i</div>
+			<div>empty</div>
+		</Flex>
+
+		<Flex vertical='horizontal'>
+			<Title>{data?.TI}</Title>
+		</Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+		<Flex vertical='horizontal'></Flex>
+	 </Flex>
   );
 }
 
