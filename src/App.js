de13e7e14639b226ee0e0f16d7f78622e9285734
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-04-18 21:47:11 +0300

    Add elements time, reach, top traffic, sentiment

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ const { Title } = Typography;
 
 function App() {
   return (
-     <Flex vertical>
-		<Flex vertical='horizontal'>
-			<time datetime={data?.DP}>{data?.DP}</time>
-			<div>Reach</div>
-			<div>Top Traffic</div>
-			<div>Sentiment</div>
-			<div>i</div>
-			<div>empty</div>
+  	<Flex vertical>
+		<Flex justify='space-between'>
+			<Flex gap='middle'>
+				<time datetime={data?.DP}>Time</time>
+				<div>Reach</div>
+				<div>Top Traffic</div>
+			</Flex>
+
+			<Flex gap='small'>
+				<div>Sentiment</div>
+				<div>i-square</div>
+				<div>empty-square</div>
+			</Flex>			
 		</Flex>
 
 		<Flex vertical='horizontal'>
 			<Title>{data?.TI}</Title>
 		</Flex>
+
 		<Flex vertical='horizontal'></Flex>
 		<Flex vertical='horizontal'></Flex>
 		<Flex vertical='horizontal'></Flex>
@@ -24,7 +30,7 @@ function App() {
 		<Flex vertical='horizontal'></Flex>
 		<Flex vertical='horizontal'></Flex>
 		<Flex vertical='horizontal'></Flex>
-	 </Flex>
+	</Flex>
   );
 }
 
