c4a17a1ed493fafef5cc95ca73dc3681407d353e
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-01 03:49:14 +0300

    Add Paragraph and Button into App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+import React from 'react';
 import { Button, Flex, Tag, Typography } from 'antd';
 import { data } from './data.js';
 import HeaderOfNews from './components/widgets/headerOfNews/headerOfNews.jsx';
 import './app.module.scss';
+import { CaretDownFilled } from '@ant-design/icons';
 
-const { Text } = Typography;
+const { Paragraph, Text } = Typography;
+
+
 
 
 function App() {
+
   return (
   	<Flex vertical gap='middle'>
 		<HeaderOfNews fullHeader />
 
-		<Flex vertical>
-			<Text>{data?.AB}</Text>
-			<Button>Show more</Button>
+		<Flex vertical align='start'>
+			<Paragraph style={{ fontSize: '20px' }}>
+				{data?.AB}
+			</Paragraph>
+
+			<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>Show more</Button>
 		</Flex>
 
 		<Flex justify='start' gap='small'>
