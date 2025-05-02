2a23e9e970e4f98ebcabc7c615756d7c94484b14
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-02 15:58:19 +0300

    Add corrections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Flex, Typography } from 'antd';
 import { data } from './data.js';
 import HeaderOfNews from './components/widgets/headerOfNews/headerOfNews.jsx';
-import './app.module.scss';
+import s from'./app.module.scss';
 import { CaretDownFilled } from '@ant-design/icons';
 import Tags from './components/shared/tags/tags.jsx';
 
@@ -22,7 +22,9 @@ function App() {
 				{data?.AB}
 			</Paragraph>
 
-			<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>Show more</Button>
+			<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>
+				<span className={s.showMoreBtn}>Show more</span>
+			</Button>
 		</Flex>
 
 		<Tags />
