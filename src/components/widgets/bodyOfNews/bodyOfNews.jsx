184c67f924227160a9b95cfc6f5e89b5bc256d50
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-05 02:36:52 +0300

    Modify BodyOfNews

--- src/components/widgets/bodyOfNews/bodyOfNews.jsx
+++ src/components/widgets/bodyOfNews/bodyOfNews.jsx
@@ -7,18 +7,23 @@ import Tags from '../../shared/tags/tags.jsx';
 
 function BodyOfNews () {
 	return (
-		<Flex vertical gap='middle'>
+		<Flex vertical gap='large'>
 			<Flex vertical align='start'>
-				<p>{data?.AB}</p>
+				<p className={s.text}>{data?.AB}</p>
 
-				<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>
+				<Button
+					type='text' 
+					icon={<CaretDownFilled style={{color: '#0050b3'}}/>}
+					iconPosition='end'
+					style={{ padding: '0' }}
+				>
 					<span className={s.showMoreBtn}>Show more</span>
 				</Button>
 			</Flex>
 
 			<Tags />
 			
-			<Button variant='solid' className={s.blueBtn}>Original Source</Button>
+			<Button variant='filled' className={s.blueBtn}>Original Source</Button>
 		</Flex>
 		
 	)
