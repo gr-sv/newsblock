b4fa7fe48991dfea4dddd153762e82f38e79ce18
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-04 17:27:32 +0300

    Add components BodyOfNews

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Button, Flex } from 'antd';
-import { data } from './data.js';
 import HeaderOfNews from './components/widgets/headerOfNews/headerOfNews.jsx';
+import BodyOfNews from './components/widgets/bodyOfNews/bodyOfNews.jsx';
 import s from'./app.module.scss';
-import { CaretDownFilled } from '@ant-design/icons';
-import Tags from './components/shared/tags/tags.jsx';
-
-
-
 
 function App() {
 
@@ -15,18 +10,8 @@ function App() {
   	<Flex vertical gap='middle' align='start'>
 		<HeaderOfNews fullHeader />
 
-		<Flex vertical align='start'>
-			<p>{data?.AB}</p>
+		<BodyOfNews />
 
-			<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>
-				<span className={s.showMoreBtn}>Show more</span>
-			</Button>
-		</Flex>
-
-		<Tags />
-
-		<Button variant='solid' className={s.blueBtn}>Original Source</Button>
-		
 		<Flex justify='space-between' style={{ width: '100%' }}>
 			<p className={s.duplicates}>Duplicates:</p>
 			<div>By Relevance</div>
---
+++ src/components/widgets/bodyOfNews/bodyOfNews.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { data } from '../../../data.js';
+import { Button, Flex } from 'antd';
+import s from './bodyOfNews.module.scss';
+import { CaretDownFilled } from '@ant-design/icons';
+import Tags from '../../shared/tags/tags.jsx';
+
+function BodyOfNews () {
+	return (
+		<>
+			<Flex vertical align='start'>
+				<p>{data?.AB}</p>
+
+				<Button type='text' icon={<CaretDownFilled />} iconPosition='end'>
+					<span className={s.showMoreBtn}>Show more</span>
+				</Button>
+			</Flex>
+
+			<Tags />
+			
+			<Button variant='solid' className={s.blueBtn}>Original Source</Button>
+		</>
+		
+	)
+}
+
+export default BodyOfNews;
