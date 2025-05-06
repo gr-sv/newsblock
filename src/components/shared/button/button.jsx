6ae0f086749123519d154e23324d92ba9d596c92
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-06 13:29:22 +0300

    Add styles for Button, corrections

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import classNames from 'classnames';
+import s from './button.module.scss';
 
-function Button ({ className, children, showMore, source, sorting, duplicates, ...otherProps }) {
+function Button ({
+	className,
+	children,
+	button_showMore,
+	button_source,
+	button_sorting,
+	button_duplicates,
+	...otherProps }) {
 	return (
 		<button
 			{...otherProps}
 			className={classNames(s.button, className, {
 				[s.button_showMore]: button_showMore,
 				[s.button_source]: button_source,
-				[s.button_sorting]: button_sorting,
-				[duplicates]: button_duplicates,
+				// [s.button_sorting]: button_sorting,
+				// [duplicates]: button_duplicates,
 			})}>
 			{ children }
 		</button>
--- src/components/widgets/bodyOfNews/bodyOfNews.jsx
+++ src/components/widgets/bodyOfNews/bodyOfNews.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { data } from '../../../data.js';
-import { Button, Flex } from 'antd';
+import { Flex } from 'antd';
 import s from './bodyOfNews.module.scss';
 import { CaretDownFilled } from '@ant-design/icons';
 import Tags from '../../shared/tags/tags.jsx';
+import Button from '../../shared/button/button.jsx';
 
 function BodyOfNews () {
 	return (
@@ -11,19 +12,16 @@ function BodyOfNews () {
 			<Flex vertical align='start'>
 				<p className={s.text}>{data?.AB}</p>
 
-				<Button
-					type='text' 
-					icon={<CaretDownFilled style={{color: '#0050b3'}}/>}
-					iconPosition='end'
-					style={{ padding: '0' }}
-				>
-					<span className={s.showMoreBtn}>Show more</span>
+				<Button button_showMore>
+					Show more <CaretDownFilled />
 				</Button>
 			</Flex>
 
 			<Tags />
-			
-			<Button variant='filled' className={s.blueBtn}>Original Source</Button>
+
+			<Button>
+				Original Source
+			</Button>
 		</Flex>		
 	)
 }
