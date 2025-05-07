885771f44df800d1028043f82b9e6b07edd2b82d
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-07 21:32:53 +0300

    Modify Highlights, add correction

--- src/components/shared/highlights/highlights.jsx
+++ src/components/shared/highlights/highlights.jsx
@@ -3,11 +3,16 @@ import { data } from "../../../data.js";
 import Button from "../button/button.jsx";
 import { Flex } from "antd";
 import { CaretDownFilled } from '@ant-design/icons';
+import s from './highlights.module.scss';
 
 function Highlights () {
 	return (
 		<Flex vertical align='start'>
-			<p>{data?.AB}</p>
+			<p>
+				{(data?.HIGHLIGHTS).map((element) => (
+					<span key={element} className={s.text}>{element}; </span>
+				))}
+			</p>
 
 			<Button button_showMore>
 				Show more <CaretDownFilled />
