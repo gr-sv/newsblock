c56a5cffd4f1132bfdf630dfa7186192a9e223f0
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-09 00:11:42 +0300

    Add event on the Button

--- src/components/shared/highlights/highlights.jsx
+++ src/components/shared/highlights/highlights.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { data } from "../../../data.js";
 import Button from "../button/button.jsx";
 import { Flex, Typography } from 'antd';
-import { CaretDownFilled } from '@ant-design/icons';
+// import { CaretDownFilled } from '@ant-design/icons';
 import s from './highlights.module.scss';
 
 const { Paragraph } = Typography;
@@ -10,23 +10,24 @@ const { Paragraph } = Typography;
 function Highlights () {
 
 	const [ellipsis, setEllipsis] = useState(true);
+	const [text, setText] = useState('Show more');
 
+	function changeButton () {
+		setEllipsis(!ellipsis);
+		setText('Show less');
+	}
 
 	return (
 		<Flex vertical align='start'>
 			<Paragraph className={s.text} ellipsis={ellipsis ? { rows: 3, expandable: true, symbol: '' } : false}>
-				
 				{(data?.HIGHLIGHTS).join('; ')}
-
 			</Paragraph>
 
 			<Button
 				button_showMore
-				onClick={() => (
-					setEllipsis(!ellipsis)
-				)}
+				onClick={changeButton}
 			>
-				Show more <CaretDownFilled />
+				{text}
 			</Button>
 		</Flex>
 	)
