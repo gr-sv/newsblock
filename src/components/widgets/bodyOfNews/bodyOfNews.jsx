80f7f1bf3cf8befad0fd10be57d935bf4242471c
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-10 15:33:59 +0300

    Add corrections

--- src/components/widgets/bodyOfNews/bodyOfNews.jsx
+++ src/components/widgets/bodyOfNews/bodyOfNews.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-// import { data } from '../../../data.js';
 import { Flex } from 'antd';
-// import s from './bodyOfNews.module.scss';
-// import { CaretDownFilled } from '@ant-design/icons';
 import Tags from '../../shared/tags/tags.jsx';
 import Button from '../../shared/button/button.jsx';
 import Highlights from '../../shared/highlights/highlights.jsx';
@@ -10,16 +7,7 @@ import Highlights from '../../shared/highlights/highlights.jsx';
 function BodyOfNews () {
 	return (
 		<Flex vertical gap='large'>
-
 			<Highlights />
-
-			{/* <Flex vertical align='start'>
-				<p className={s.text}>{data?.AB}</p>
-
-				<Button button_showMore>
-					Show more <CaretDownFilled />
-				</Button>
-			</Flex> */}
 
 			<Tags />
 
