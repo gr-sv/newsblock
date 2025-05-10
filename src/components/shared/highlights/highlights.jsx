2b3686bfd93d6e89d3a2c041017d3d44fe0c0964
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-05-10 15:15:23 +0300

    Add function buttonClick

--- src/components/shared/highlights/highlights.jsx
+++ src/components/shared/highlights/highlights.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { data } from "../../../data.js";
 import Button from "../button/button.jsx";
 import { Flex, Typography } from 'antd';
-import { CaretDownFilled } from '@ant-design/icons';
+import { CaretDownFilled, CaretUpFilled } from '@ant-design/icons';
 import s from './highlights.module.scss';
 
 const { Paragraph } = Typography;
@@ -10,18 +10,24 @@ const { Paragraph } = Typography;
 function Highlights () {
 
 	const [ellipsis, setEllipsis] = useState(true);
+	const [buttonText, setButtonText] = useState('Show more');
+
+	const buttonClick = () => {
+		setButtonText(buttonText === 'Show more' ? 'Show less' : 'Show more');
+		setEllipsis(!ellipsis);
+	};
 
 	return (
 		<Flex vertical align='start'>
 			<Paragraph className={s.text} ellipsis={ellipsis ? { rows: 3, expandable: true, symbol: '' } : false}>
 				{(data?.HIGHLIGHTS).join('; ')}
 			</Paragraph>
-
+			
 			<Button
 				button_showMore
-				onClick={() => setEllipsis(!ellipsis)}
+				onClick={buttonClick}
 			>
-				Show more < CaretDownFilled/>
+				{buttonText}
 			</Button>
 		</Flex>
 	)
